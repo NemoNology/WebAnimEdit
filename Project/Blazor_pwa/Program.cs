05fddc22032f4ff9aba8125228c864d3e12be798
NemoNology <chicken.gang@mail.ru>
2024-03-02 21:01:53 +0900

    Project: added smart blazor package

--- Project/Blazor_pwa/Program.cs
+++ Project/Blazor_pwa/Program.cs
@@ -1,10 +1,11 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Blazor_pwa;
+using Smart.Blazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddSmart();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
