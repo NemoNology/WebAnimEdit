8812a5de0c537bdc54999e8c578cad5df918061b
NemoNology <chicken.gang@mail.ru>
2024-03-03 20:24:41 +0900

    Removed smart.blazor package

--- Project/Blazor_pwa/Program.cs
+++ Project/Blazor_pwa/Program.cs
@@ -1,11 +1,10 @@
+using Blazor_pwa;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Smart.Blazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.Services.AddSmart();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
