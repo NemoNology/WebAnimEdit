e6335110e9f293aee4d945ca9d01f8bb66ea888e
NemoNology <chicken.gang@mail.ru>
2024-03-06 19:21:49 +0900

    Root page:
    Changed:
    > Hot reload from usual to PWA
    Addded:
    + Stylesheets links

--- Project/Remix_pwa/app/root.tsx
+++ Project/Remix_pwa/app/root.tsx
@@ -2,24 +2,31 @@ import { cssBundleHref } from "@remix-run/css-bundle";
 import type { LinksFunction } from "@remix-run/node";
 import {
   Links,
-  LiveReload,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import { useSWEffect, LiveReload } from "@remix-pwa/sw";
+
+import TWStyles from "~/tailwind.css";
+import AppStyles from "~/app.css";
 
 export const links: LinksFunction = () => [
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+  { rel: "stylesheet", href: TWStyles },
+  { rel: "stylesheet", href: AppStyles },
 ];
 
 export default function App() {
+  useSWEffect();
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
+        <link rel="manifest" href="/manifest.webmanifest" />
         <Links />
       </head>
       <body>
