66423e62154696e5579c17c7b54e2fe1982b51d5
NemoNology <chicken.gang@mail.ru>
2024-03-11 07:01:08 +0900

    Index page:
    Added:
    + Project title input
    + Project tree view
    + Logging html page builder building

--- Project/Remix_pwa/app/routes/_index.tsx
+++ Project/Remix_pwa/app/routes/_index.tsx
@@ -1,6 +1,11 @@
 import type { MetaFunction } from "@remix-run/node";
-import Dropdown from "~/components/Dropdown";
 import Nav from "~/components/Nav";
+import Button from "~/components/Button";
+import Main from "~/components/Main";
+import PropertyEditor from "~/components/PropertyEditor";
+import { Project } from "~/Models/implementations/Project";
+import { HtmlDocumentBuilder } from "~/Models/implementations/HtmlDocumentBuilder";
+import ProjectTree from "~/components/ProjectTree";
 
 export const meta: MetaFunction = () => {
   return [
@@ -10,16 +15,36 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
-  return (
-    <body className="flex flex-col h-100 w-100">
-      <Nav>
-        {/* <Dropdown Header="Проект" DropdownMenu={
-          
-        }/> */}
-      </Nav>
-      <main>
+  const project: Project = new Project();
+  const hdb: HtmlDocumentBuilder = new HtmlDocumentBuilder();
 
-      </main>
+  return (
+    <body className="flex flex-col h-full">
+      <Nav Children={
+        <>
+          <Button Header="Новый" />
+          <Button Header="Экспорт" />
+        </>
+      } />
+      <Main Children={
+        <>
+          <div className="flex flex-col p-2 flex-grow bg-secondary justify-center">
+            <PropertyEditor PropertyName="Заголовок"
+              PropertyValue={project.Title}
+              onChange={(value: any) => {
+                project.Title = value;
+              }} />
+            <ProjectTree Project={project} />
+          </div>
+          <div className="flex justify-between" style={{ flexGrow: 3.5 }}>
+            <Button Header="Log build"
+              onClick={() => console.log(hdb.Build(project.Title, project.Elements, project.Animations))} />
+          </div>
+          <div className="flex flex-grow bg-secondary justify-center">
+
+          </div>
+        </>
+      } />
     </body>
   );
 }
