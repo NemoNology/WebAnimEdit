0e4009d79a028d37aab3051c186bc93a0d672a6c
NemoNology <chicken.gang@mail.ru>
2024-03-13 20:29:10 +0900

    Animation class - B 0.5
    Added:
    + Init style properties

--- Project/Blazor_pwa/Models/Implementations/Animation.cs
+++ Project/Blazor_pwa/Models/Implementations/Animation.cs
@@ -3,11 +3,16 @@ namespace Blazor_pwa.Models.Implementations
     public class Animation
     {
         public string Name { get; set; }
-        public List<KeyFrame> KeyFrames { get; init; } = new();
+        public Dictionary<string, string> Transform { get; private set; } = new();
 
         public Animation(string name)
         {
             Name = name;
+            Transform.Add("translateX", "0px");
+            Transform.Add("translateY", "0px");
+            Transform.Add("scaleX", "1");
+            Transform.Add("scaleY", "1");
+            Transform.Add("rotate", "0deg");
         }
     }
 }
