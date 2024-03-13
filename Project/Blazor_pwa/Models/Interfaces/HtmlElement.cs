7f1e9dddce19a3653116d293c936086d7a15e12c
NemoNology <chicken.gang@mail.ru>
2024-03-13 19:55:12 +0900

    Html element class - B 0.4
    Changed:
    > Changed to class
    Added:
    + Constructor

---
+++ Project/Blazor_pwa/Models/Interfaces/HtmlElement.cs
@@ -0,0 +1,34 @@
+using Blazor_pwa.Models.Implementations;
+
+namespace Blazor_pwa.Models.Interfaces
+{
+    public abstract class HtmlElement
+    {
+        /// <summary>
+        /// Name (Tag) of the HTML element
+        /// </summary>
+        public string Name { get; protected set; }
+        /// <summary>
+        /// Dictionary of element parameters as key-value pairs
+        /// </summary>
+        public Dictionary<string, string> Attributes { get; protected set; } = new();
+        /// <summary>
+        /// Dictionary of element style properties as key-value pairs
+        /// </summary>
+        public Dictionary<string, string> Style { get; protected set; } = new();
+        /// <summary>
+        /// List of element animations
+        /// </summary>
+        public Animation? Animation { get; protected set; } = null;
+
+        public HtmlElement(string name)
+        {
+            Name = name;
+            Style.Add("left", "0px");
+            Style.Add("top", "0px");
+            Style.Add("position", "absolute");
+            Style.Add("z-index", "0");
+            Style.Add("animation-duration", "1s");
+        }
+    }
+}
