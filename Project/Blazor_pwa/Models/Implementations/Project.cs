f1e86301c56fee98e535e5fc3c3979c10a6ae4e4
NemoNology <chicken.gang@mail.ru>
2024-03-14 04:59:32 +0900

    Added project class - B 0.25

---
+++ Project/Blazor_pwa/Models/Implementations/Project.cs
@@ -0,0 +1,16 @@
+using Blazor_pwa.Models.Interfaces;
+
+namespace Blazor_pwa.Models.Implementations
+{
+    public class Project
+    {
+        public string Title { get; set; }
+        public List<HtmlElement> Elements { get; private set; } = new();
+        public List<Animation> Animations { get; private set; } = new();
+
+        public Project(string title)
+        {
+            Title = title;
+        }
+    }
+}
