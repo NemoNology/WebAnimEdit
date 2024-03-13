97881c1212c4357eeb8d38e3e68609bf993688b9
NemoNology <chicken.gang@mail.ru>
2024-03-14 04:59:53 +0900

    Added html document builder class - B 0.4

---
+++ Project/Blazor_pwa/Models/Implementations/HtmlDocumentBuilder.cs
@@ -0,0 +1,58 @@
+using Blazor_pwa.Models.Interfaces;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace Blazor_pwa.Models.Implementations
+{
+    public class HtmlDocumentBuilder
+    {
+        static string GetHeadWithTitle(string title) =>
+            $@"<head>
+                <meta charset={"UTF-8"}
+                <meta name={"viewport"} content={"width=device-width, initial-scale=1.0"}
+                <meta http-equiv={"X-UA-Compatible"} content={"ie=edge"}
+                <title>{title}</title>;
+            </head>";
+
+        static string BuildAnimation(Animation animation)
+        {
+            string res = $"@keyframes {animation.Name} {{\n to {{";
+            string transformInfo = "";
+            foreach ((string key, string value) in animation.Transform)
+                transformInfo += $"{key}: {value}; ";
+            if (animation.Transform.Count > 0)
+                res += $"transform: ${transformInfo}; ";
+            res += $"}}\n}}\n";
+            return res;
+        }
+
+        static string BuildElement(HtmlElement element)
+        {
+            string res = $"<{element.Name} ";
+            foreach ((string key, string value) in element.Attributes)
+                res += $"{key}: {value}; ";
+            string styleInfo = "";
+            foreach ((string key, string value) in element.Style)
+                styleInfo += $"{key}: {value}; ";
+            if (element.Style.Count > 0)
+            {
+                res += $"style=\"{styleInfo} ";
+                if (element.Animation is not null)
+                    res += $"animation-name: {element.Animation.Name} ";
+                res += "\";";
+            }
+            return res;
+        }
+
+        static string BuildProject(Project project) =>
+            @$"<DOCKTYPE html>
+<html lang=""en"">
+    <style>
+        {project.Elements.Select(el => el.Animation is null ? "" : BuildAnimation(el.Animation))}
+    </style>
+    {GetHeadWithTitle(project.Title)}
+    <body>
+        {project.Elements.Select(el => BuildElement(el) + "\n")}
+    </body>
+</html>";
+    }
+}
