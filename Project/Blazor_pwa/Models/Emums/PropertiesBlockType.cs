4b677f1851481d7d68cd80a96e98f6d2721b4ef3
NemoNology <chicken.gang@mail.ru>
2024-03-27 23:21:27 +0900

    Added new "properties block type" enum

---
+++ Project/Blazor_pwa/Models/Emums/PropertiesBlockType.cs
@@ -0,0 +1,11 @@
+namespace Blazor_pwa.Models.Emums
+{
+    public enum PropertiesBlockType
+    {
+        Attributes,
+        Style,
+        Classes,
+        Animations,
+        EventHandlers,
+    }
+}
