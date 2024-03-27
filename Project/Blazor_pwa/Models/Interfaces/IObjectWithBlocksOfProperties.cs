6cbffadb87381da63dd4342e549140b4f6794987
NemoNology <chicken.gang@mail.ru>
2024-03-28 04:09:49 +0900

    Object with blocks of properties interface
    Added:
    + Documentation for "Blocks" property

--- Project/Blazor_pwa/Models/Interfaces/IObjectWithBlocksOfProperties.cs
+++ Project/Blazor_pwa/Models/Interfaces/IObjectWithBlocksOfProperties.cs
@@ -2,6 +2,9 @@ namespace Blazor_pwa.Models.Interfaces
 {
     public interface IObjectWithBlocksOfProperties
     {
+        /// <summary>
+        /// Object blocks of properties
+        /// </summary>
         public BlockOfProperties[] Blocks { get; }
     }
 }
