7870e979e72d18a6637991a2cbc971c4934a8777
NemoNology <chicken.gang@mail.ru>
2024-03-28 03:16:39 +0900

    "Block of properties" class:
    Added:
    + Full constructor
    Changed:
    > Abstract class to not abstract

--- Project/Blazor_pwa/Models/Interfaces/BlockOfProperties.cs
+++ Project/Blazor_pwa/Models/Interfaces/BlockOfProperties.cs
@@ -2,16 +2,27 @@ using Blazor_pwa.Models.Emums;
 
 namespace Blazor_pwa.Models.Interfaces
 {
-    public abstract class BlockOfProperties
+    public class BlockOfProperties
     {
         /// <summary>
         /// Type of the block. Type is used to identify the block
         /// </summary>
-        public abstract BlockOfPropertiesType Type { get; }
+        public BlockOfPropertiesType Type { get; private set; }
 
         /// <summary>
         /// Dictionary of block properties, where property is name and value
         /// </summary>
-        public Dictionary<string, string> Properties { get; private set; } = new();
+        public Dictionary<string, string> Properties { get; private set; }
+
+        /// <summary>
+        /// Constructor with type and properties parameters
+        /// </summary>
+        /// <param name="type">Type of the block</param>
+        /// <param name="properties">Initial properties for block. If null, new empty dictionary will be created</param>
+        public BlockOfProperties(BlockOfPropertiesType type, Dictionary<string, string>? properties = null)
+        {
+            Type = type;
+            Properties = properties ?? new();
+        }
     }
 }
