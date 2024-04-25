53fd70293aff0c5cdd919ee9654730b5e738d3bb
NemoNology <chicken.gang@mail.ru>
2024-04-25 15:12:30 +0900

    Added property value type enum

---
+++ Blazor_pwa_project/Models/Enums/PropertyValueType.cs
@@ -0,0 +1,12 @@
+namespace Blazor_pwa_project.Models.Enums
+{
+    public enum PropertyValueType
+    {
+        None,
+        Value,
+        Attribute,
+        Style,
+        Script,
+        Event,
+    }
+}
