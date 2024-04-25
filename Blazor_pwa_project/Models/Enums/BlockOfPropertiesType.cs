32a223b6a2f6e82490894a38a1b6d240f91fd5d8
NemoNology <chicken.gang@mail.ru>
2024-04-25 15:17:08 +0900

    Block of properties type enum
    Added
    + Text & Transform types
    Changed
    > `Event handler` type name to `Events`

--- Blazor_pwa_project/Models/Enums/BlockOfPropertiesType.cs
+++ Blazor_pwa_project/Models/Enums/BlockOfPropertiesType.cs
@@ -3,9 +3,14 @@ namespace Blazor_pwa.Models.Emums
     public enum BlockOfPropertiesType
     {
         Attributes,
+        Text,
+        Transform,
         Style,
+        /// <summary>
+        /// Will be removed soon
+        /// </summary>
         AnimationStyle,
-        EventHandler,
+        Events,
         StyleSheet,
         Script,
     }
