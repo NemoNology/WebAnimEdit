f75a3a03137a8f2f84c67d7992e127cd586e3751
NemoNology <chicken.gang@mail.ru>
2024-04-27 07:59:44 +0900

    Attributes class
    Added
    + Text attribute
    Changed
    > Namespaces collision resolving simplified

--- Blazor_pwa_project/Models/Enums/Attributes.cs
+++ Blazor_pwa_project/Models/Enums/Attributes.cs
@@ -1,170 +1,177 @@
+using Attribute = WebAnimEdit.Models.Implementations.Attribute;
+
 namespace WebAnimEdit.Models.Enums
 {
-    public static class Properties
+    public static class Attributes
     {
         public const string STYLEATTRIBUTETAG = "style";
         public const string ANIMATIONSTYLEATTRIBUTETAG = "to";
 
-        public static Implementations.Attribute FontSize => new(
+        public static Attribute Text => new(
+            "Текст",
+            "",
+            "",
+            AttributeValueType.None);
+        public static Attribute FontSize => new(
             "Размер шрифта",
             "font-size",
             "14px",
             AttributeValueType.Value,
             STYLEATTRIBUTETAG);
-        public static Implementations.Attribute FontFamily => new(
+        public static Attribute FontFamily => new(
             "Семейство шрифтов",
             "font-family",
             "\"Times New Roman\", Times, serif",
             AttributeValueType.None,
             STYLEATTRIBUTETAG);
-        public static Implementations.Attribute FontStyle => new(
+        public static Attribute FontStyle => new(
             "Стиль шрифта",
             "font-style",
             "normal",
             AttributeValueType.Value,
             STYLEATTRIBUTETAG);
-        public static Implementations.Attribute FontWeight => new(
+        public static Attribute FontWeight => new(
             "Толщина шрифта",
             "font-weight",
             "normal",
             AttributeValueType.Value,
             STYLEATTRIBUTETAG);
-        public static Implementations.Attribute FontColor => new(
+        public static Attribute FontColor => new(
             "Цвет шрифта",
             "color",
             "black",
             AttributeValueType.Value,
             STYLEATTRIBUTETAG);
-        public static Implementations.Attribute BackgroundColor => new(
+        public static Attribute BackgroundColor => new(
             "Цвет заднего фона",
             "background-color",
             "unset",
             AttributeValueType.Value,
             STYLEATTRIBUTETAG);
-        public static Implementations.Attribute Padding => new(
+        public static Attribute Padding => new(
             "Padding",
             "padding",
             "unset",
             AttributeValueType.Value,
             STYLEATTRIBUTETAG);
-        public static Implementations.Attribute Margin => new(
+        public static Attribute Margin => new(
             "Margin",
             "margin",
             "unset",
             AttributeValueType.Value,
             STYLEATTRIBUTETAG);
-        public static Implementations.Attribute BorderThickness => new(
+        public static Attribute BorderThickness => new(
             "Толщина обводки",
             "border-width",
             "unset",
             AttributeValueType.Value,
             STYLEATTRIBUTETAG);
-        public static Implementations.Attribute BorderColor => new(
+        public static Attribute BorderColor => new(
             "Цвет обводки",
             "border-color",
             "unset",
             AttributeValueType.Value,
             STYLEATTRIBUTETAG);
-        public static Implementations.Attribute BorderStyle => new(
+        public static Attribute BorderStyle => new(
             "Стиль обводки",
             "border-style",
             "solid",
             AttributeValueType.Value,
             STYLEATTRIBUTETAG);
-        public static Implementations.Attribute BorderCornerRadius => new(
+        public static Attribute BorderCornerRadius => new(
             "Закругление обводки",
             "border-radius",
             "unset",
             AttributeValueType.Value,
             STYLEATTRIBUTETAG);
-        public static Implementations.Attribute PositionType => new(
+        public static Attribute PositionType => new(
             "Тип расположения",
             "position",
             "relative",
             AttributeValueType.Value,
             STYLEATTRIBUTETAG);
-        public static Implementations.Attribute PositionLeft => new(
+        public static Attribute PositionLeft => new(
             "Положение X",
             "left",
             "0px",
             AttributeValueType.Value,
             STYLEATTRIBUTETAG);
-        public static Implementations.Attribute PositionTop => new(
+        public static Attribute PositionTop => new(
             "Положение Y",
             "top",
             "0px",
             AttributeValueType.Value,
             STYLEATTRIBUTETAG);
-        public static Implementations.Attribute Width => new(
+        public static Attribute Width => new(
             "Ширина",
             "width",
             "auto",
             AttributeValueType.Value,
             STYLEATTRIBUTETAG);
-        public static Implementations.Attribute Height => new(
+        public static Attribute Height => new(
             "Высота",
             "height",
             "auto",
             AttributeValueType.Value,
             STYLEATTRIBUTETAG);
-        public static Implementations.Attribute Rotate => new(
+        public static Attribute Rotation => new(
             "Поворот",
             "rotate",
             "none",
             AttributeValueType.Value,
             STYLEATTRIBUTETAG);
-        public static Implementations.Attribute Scale => new(
+        public static Attribute Scale => new(
             "Маштаб",
             "scale",
             "none",
             AttributeValueType.Value,
             STYLEATTRIBUTETAG);
-        public static Implementations.Attribute Class => new(
+        public static Attribute Class => new(
             "Классы таблицы стилей",
             "class",
             "",
             AttributeValueType.None);
-        public static Implementations.Attribute Style => new(
+        public static Attribute Style => new(
             "Стиль",
             STYLEATTRIBUTETAG,
             "",
             AttributeValueType.Style);
-        public static Implementations.Attribute AnimationStyle => new(
+        public static Attribute AnimationStyle => new(
             "Стиль анимации",
             ANIMATIONSTYLEATTRIBUTETAG,
             "",
             AttributeValueType.Style);
-        public static Implementations.Attribute OnClick => new(
+        public static Attribute OnClick => new(
             "Обработчик события клика",
             "onclick",
             "",
             AttributeValueType.Script);
-        public static Implementations.Attribute OnMouseOver => new(
+        public static Attribute OnMouseOver => new(
             "Обрабочик события наведения курсора",
             "onmouseover",
             "",
             AttributeValueType.Script);
-        public static Implementations.Attribute OnMouseOut => new(
+        public static Attribute OnMouseOut => new(
             "Обрабочик события отведения курсора",
             "onmouseout",
             "",
             AttributeValueType.Script);
-        public static Implementations.Attribute OnKeyDown => new(
+        public static Attribute OnKeyDown => new(
             "При нажатии на клавишу",
             "onkeydown",
             "",
             AttributeValueType.Script);
-        public static Implementations.Attribute StyleSheet => new(
+        public static Attribute StyleSheet => new(
             "Таблица стилей",
             "",
             "",
             AttributeValueType.Style);
-        public static Implementations.Attribute Script => new(
+        public static Attribute Script => new(
             "JavaScript скрипт",
             "",
             "",
             AttributeValueType.Script);
-        public static Implementations.Attribute ImageSource => new(
+        public static Attribute ImageSource => new(
             "URL-источник изображения",
             "src",
             "",
