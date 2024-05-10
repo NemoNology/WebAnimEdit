a723d2aaed36d3bc151c49fc758d6de72e25d456
NemoNology <chicken.gang@mail.ru>
2024-05-10 12:16:57 +0900

    Html element extensions static class
    Added
    + Method for setting attributes values;
    + Method for getting specified elements (html) for specified attribute value types. This is needed for code auto complete;
    + Html element to Xml element method;
    + Html element to html element (code) method;

--- Blazor_pwa_project/Models/Implementations/Supportive classes/HtmlElementExtentions.cs
+++ Blazor_pwa_project/Models/Implementations/Supportive classes/HtmlElementExtentions.cs
@@ -1,3 +1,7 @@
+using System.Xml;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
 namespace WebAnimEdit.Models.Implementations
 {
     public static class HtmlElementExtentions
@@ -33,5 +37,79 @@ namespace WebAnimEdit.Models.Implementations
                 }
             }
         }
+
+        public static HtmlElement WithAnotherAttributeValueByAttributeTag(
+            this HtmlElement element,
+            string attributeTag,
+            string newAttributeValue)
+        {
+            foreach (var attribute in element.Attributes)
+            {
+                if (attribute.Tag == attributeTag)
+                {
+                    attribute.Value = newAttributeValue;
+                    return element;
+                }
+            }
+
+            return element;
+        }
+
+        public static RenderFragment GetSpecifiedInputFieldByValueType(this Attribute attribute)
+        => attribute.ValueType switch
+        {
+            _ => builder =>
+            {
+                builder.OpenComponent<MudTextField<string>>(1);
+                builder.AddAttribute(2, "Value",
+                    EventCallback.Factory.CreateBinder<string>(
+                        attribute, value => attribute.Value = value, attribute.Value));
+                builder.AddAttribute(3, "Variant", Variant.Text);
+                builder.AddAttribute(4, "Label", attribute.Name);
+                builder.CloseComponent();
+            }
+        };
+
+        public static XmlElement ToXml(this HtmlElement element, XmlDocument? doc = null)
+        {
+            doc ??= new();
+            var el = doc.CreateElement(element.Tag);
+            foreach (var attribute in element.Attributes)
+            {
+                bool hasParrentAttribute = !string.IsNullOrWhiteSpace(attribute.ParentAttributeTag);
+                XmlAttribute attrBuffer;
+                if (!hasParrentAttribute && string.IsNullOrWhiteSpace(attribute.Tag))
+                {
+                    el.InnerText = attribute.Value;
+                    continue;
+                }
+                else if (hasParrentAttribute)
+                {
+                    attrBuffer = (el.Attributes.GetNamedItem(attribute.ParentAttributeTag!) as XmlAttribute)!;
+                    if (attrBuffer is null)
+                    {
+                        attrBuffer = doc.CreateAttribute(attribute.ParentAttributeTag!);
+                        attrBuffer.Value = $"{attribute.Tag}: {attribute.Value};";
+                    }
+                    else
+                    {
+                        attrBuffer.Value += $" {attribute.Tag}: {attribute.Value};";
+                    }
+                }
+                else
+                {
+                    attrBuffer = doc.CreateAttribute(attribute.Tag);
+                    attrBuffer.Value = attribute.Value;
+                }
+                el.Attributes.Append(attrBuffer);
+            }
+            foreach (var child in element.Children)
+                el.AppendChild(child.ToXml(doc));
+
+            return el;
+        }
+
+        public static string ToHtml(this HtmlElement element)
+        => element.ToXml().OuterXml;
     }
 }
