7e2dca49a2c774bbdd7f6000d70aa4b09b821cdb
NemoNology <chicken.gang@mail.ru>
2024-05-17 08:07:14 +0900

    Html elements static class. Updated; Added some patterns

--- Blazor_pwa_project/Models/Enums/HtmlElements.cs
+++ Blazor_pwa_project/Models/Enums/HtmlElements.cs
@@ -5,104 +5,91 @@ namespace WebAnimEdit.Models.Enums
 {
     public static class HtmlElements
     {
-        public static HashSet<Attribute> DivisionAttributes =>
+        public static HashSet<BlockOfAttributes> DivisionBlocksOfAttributes =>
             new()
             {
-                Attributes.Text,
-                Attributes.FontSize,
-                Attributes.FontFamily,
-                Attributes.FontStyle,
-                Attributes.FontWeight,
-                Attributes.FontColor,
-                Attributes.BackgroundColor,
-                Attributes.Padding,
-                Attributes.Margin,
-                Attributes.BorderThickness,
-                Attributes.BorderColor,
-                Attributes.BorderStyle,
-                Attributes.BorderCornerRadius,
-                Attributes.PositionType,
-                Attributes.PositionLeft,
-                Attributes.PositionTop,
-                Attributes.Width,
-                Attributes.Height,
-                Attributes.Rotation,
-                Attributes.Scale,
-                Attributes.Class,
-                Attributes.Style,
-                Attributes.AnimationStyle,
-                Attributes.OnClick,
-                Attributes.OnMouseOver,
-                Attributes.OnMouseOut,
-                Attributes.OnKeyDown
+                BlocksOfAttributes.Text,
+                BlocksOfAttributes.Shape,
+                BlocksOfAttributes.Transform,
+                BlocksOfAttributes.Style,
+                BlocksOfAttributes.Event,
             };
 
         public static HtmlElement Division => new(
             "Разделитель",
             "div",
-            DivisionAttributes);
+            DivisionBlocksOfAttributes);
         public static HtmlElement Image => new(
             "Изображение",
             "div",
             new()
             {
-                Attributes.ImageSource,
-                Attributes.BackgroundColor,
-                Attributes.Padding,
-                Attributes.Margin,
-                Attributes.BorderThickness,
-                Attributes.BorderColor,
-                Attributes.BorderStyle,
-                Attributes.BorderCornerRadius,
-                Attributes.PositionType,
-                Attributes.PositionLeft,
-                Attributes.PositionTop,
-                Attributes.Width,
-                Attributes.Height,
-                Attributes.Rotation,
-                Attributes.Scale,
-                Attributes.Class,
-                Attributes.Style,
-                Attributes.AnimationStyle,
-                Attributes.OnClick,
-                Attributes.OnMouseOver,
-                Attributes.OnMouseOut,
-                Attributes.OnKeyDown
+                BlocksOfAttributes.Image,
+                BlocksOfAttributes.Shape,
+                BlocksOfAttributes.Transform,
+                BlocksOfAttributes.Style,
+                BlocksOfAttributes.Event,
             });
         public static HtmlElement Button => new(
             "Кнопка",
             "button",
-            DivisionAttributes);
-        public static HtmlElement Html => new(
+            DivisionBlocksOfAttributes);
+        public static HtmlElement Document => new(
             "HTML-документ",
             "html",
-            DivisionAttributes)
+            DivisionBlocksOfAttributes)
         {
             Children = new()
+            {
+                DocumentHead,
+                new("Тело документа", "body", DivisionBlocksOfAttributes)
                 {
-                    Style,
-                    new("Тело документа", "body", DivisionAttributes),
-                    JSScript
-                }
+                    Children = new()
+                    {
+                        StyleSheet
+                    }
+                },
+                JSScript
+            }
         };
-        public static HtmlElement Style => new(
+        public static HtmlElement StyleSheet => new(
             "Таблица стилей",
             "style",
             new() {
-                Attributes.StyleSheet
+                BlocksOfAttributes.StyleSheet
             });
         public static HtmlElement JSScript => new(
             "JavaScript скрипт",
             "script",
-            new() {
-                Attributes.Script
-            });
-        public static HtmlElement Input => new(
-            "Поле ввода",
-            "input",
-            DivisionAttributes.Concat(new HashSet<Attribute>() {
-                Attributes.InputType,
-                }).ToHashSet());
+            new() { BlocksOfAttributes.Script });
+        public static HtmlElement Input
+        {
+            get
+            {
+                var blocks = DivisionBlocksOfAttributes;
+                blocks.Add(BlocksOfAttributes.Input);
+                return new("Поле ввода", "input", blocks);
+            }
+        }
+        public static HtmlElement DocumentHead => new(
+            "Голова документа",
+            "head",
+new()
+        )
+        {
+            // <meta charset="UTF-8">
+            // <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            Children = new()
+            {
+                new("Мета. Кодировка", "meta", new() { new(new HashSet<Attribute>() {new("Мета. Кодировка", "charset", "UTF-8") }) }),
+                new("Мета. Viewport", "meta", new()
+                {
+                    new(new HashSet<Attribute>() {
+                        new("Мета. Содержимое", "content", "width=device-width, initial-scale=1.0"),
+                    }),
+                })
+            }
+        };
 
         public static HashSet<HtmlElement> GeneralElements => new()
         {
