e91c82cc683099c95d488e1376238affffc1221c
NemoNology <chicken.gang@mail.ru>
2024-05-17 08:06:20 +0900

    Added block of attributes class

---
+++ Blazor_pwa_project/Models/Enums/BlockOfAttributesType.cs
@@ -0,0 +1,16 @@
+namespace WebAnimEdit.Models.Enums
+{
+    public enum BlockOfAttributesType
+    {
+        Custom,
+        Text,
+        Shape,
+        Transform,
+        Style,
+        Event,
+        StyleSheet,
+        Script,
+        Input,
+        Image,
+    }
+}
